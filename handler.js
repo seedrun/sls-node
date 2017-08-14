bee378ea72f672439dfb4419d093604c40991b4c
Frank <bb@utmuser2-201.wireless.utoronto.ca>
2017-08-14 18:58:36 -0400

    Updated handler

--- handler.js
+++ handler.js
@@ -1,15 +1,7 @@
-'use strict';
-
 module.exports.helloWorld = (event, context, callback) => {
   const response = {
     statusCode: 200,
-    headers: {
-      'Access-Control-Allow-Origin': '*', // Required for CORS support to work
-    },
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
+    body: 'Hello world!',
   };
 
   callback(null, response);
