a55672327d338468238c3c9be52e95620309afac
Frank <frank@anomalyinnovations.com>
2017-08-15 16:30:48 -0400

    Initial commit

--- handler.js
+++ handler.js
@@ -1,6 +1,16 @@
+'use strict';
+
 module.exports.helloWorld = (event, context, callback) => {
-  callback(null, {
-//    body: process.env.MESSAGE + ' ' + '\n',
-    body: `${process.env.MESSAGE} Secret ${process.env.MY_SECRET}\n`
-  });
+  const response = {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*', // Required for CORS support to work
+    },
+    body: JSON.stringify({
+      message: 'Go Serverless v1.0! Your function executed successfully!',
+      input: event,
+    }),
+  };
+
+  callback(null, response);
 };
