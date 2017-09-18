5c0ceb1efc8496448c83b6a438043c64b2972096
Jay <jvelayud@gmail.com>
2017-09-18 14:40:43 -0400

    Secret vars

--- handler.js
+++ handler.js
@@ -6,7 +6,7 @@ module.exports.helloWorld = (event, context, callback) => {
     headers: {
       'Access-Control-Allow-Origin': '*',
     },
-    body: `${process.env.MESSAGE}.\n`
+    body: `${process.env.MESSAGE}, secret is ${process.env.MY_SECRET}.\n`
   };
 
   callback(null, response);
