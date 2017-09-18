6399d48d12f51a5ffd29ce00d8ed3ecac4079ac0
Jay V <air@live.ca>
2017-09-18 14:44:21 -0400

    Update handler.js

--- handler.js
+++ handler.js
@@ -6,7 +6,7 @@ module.exports.helloWorld = (event, context, callback) => {
     headers: {
       'Access-Control-Allow-Origin': '*',
     },
-    body: `${process.env.MESSAGE}, secret is ${process.env.MY_SECRET}.\n`
+    body: `This is a fork: ${process.env.MESSAGE}, secret is ${process.env.MY_SECRET}.\n`
   };
 
   callback(null, response);
