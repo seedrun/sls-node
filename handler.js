6c309a7543dfdc56699e6d82daae8ca370dbff7f
Frank <wangfanjie@gmail.com>
2017-11-30 19:50:49 -0500

    Update handler.js

--- handler.js
+++ handler.js
@@ -6,7 +6,7 @@ module.exports.main = (event, context, callback) => {
     headers: {
       'Access-Control-Allow-Origin': '*',
     },
-    body: `Hello Mm!\n`
+    body: `Hello Mmm!\n`
   };
 
   callback(null, response);
