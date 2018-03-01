895a20b38956f2c4c700c3d2c280883efccce6fa
Jay V <air@live.ca>
2018-03-01 14:45:00 -0500

    Update handler.js

--- handler.js
+++ handler.js
@@ -6,7 +6,7 @@ module.exports.helloWorld = (event, context, callback) => {
     headers: {
       'Access-Control-Allow-Origin': '*',
     },
-    body: `This a Field test: ${process.env.MESSAGE}, secret is ${process.env.MY_SECRET}.\n`
+    body: `This a demo app: ${process.env.MESSAGE}, secret is ${process.env.MY_SECRET}.\n`
   };
 
   callback(null, response);
