083ff327b0969c20078d8343e16231439e311380
seedrun <37090581+seedrun@users.noreply.github.com>
2018-03-05 19:47:07 -0500

    Update handler.js

--- handler.js
+++ handler.js
@@ -6,7 +6,7 @@ module.exports.main = (event, context, callback) => {
     headers: {
       'Access-Control-Allow-Origin': '*',
     },
-    body: `Hello ${process.env.MESSAGE}!\n`
+    body: `Hello ${process.env.PASSWORD}!\n`
   };
 
   callback(null, response);
